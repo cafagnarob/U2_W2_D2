c9b454f5ac286151c1ed6cc3f5bcc587e2e9a63e
cafagnarob <cafagna.rob@gmail.com>
2026-04-08 16:23:00 +0200

    try-js

---
+++ script.js
@@ -0,0 +1,4 @@
+const linkSaldi = document.getElementById("NascondiSaldi")
+const linkSummer = document.getElementById("linkSummer")
+
+const bottoni = document.querySelectorAll("main button, footer button")
